46cbe1e4beba9c06e4afa182c90b00ae460f13ed
Jessica Nash <janash@ncsu.edu>
2018-07-18 20:47:02 -0500

    change fomms_integrate to fomms_integrate2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DOCLINES = __doc__.split("\n")
 
 setup(
     # Self-descriptive entries which should always be present
-    name='fomms_integrate',
+    name='fomms_integrate2',
     author='Jessica A. Nash',
     description=DOCLINES[0],
     long_description="\n".join(DOCLINES[2:]),
@@ -21,12 +21,12 @@ setup(
     ],
 
     # Which Python importable modules should be included when your package is installed
-    packages=['fomms_integrate', "fomms_integrate.tests"],
+    packages=['fomms_integrate2', "fomms_integrate2.tests"],
 
     # Optional include package data to ship with your package
     # Comment out this line to prevent the files from being packaged with your software
     # Extend/modify the list to include/exclude other items as need be
-    package_data={'fomms_integrate': ["data/*.dat"]
+    package_data={'fomms_integrate2': ["data/*.dat"]
                   },
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
